9b4ea6c027dd51cc6b87671e4622b523424173f2
varscript <fvargas.edu@gmail.com>
2024-05-08 15:58:51 -0500

    search by region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CounriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,11 +11,20 @@ import { CounriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
 
   constructor(private counriesService: CounriesService) {}
 
-  searchByRegion(term: string) {
-    this.counriesService.searchRegion(term).subscribe((countries) => {
+  searchByRegion(region: Region) {
+    this.selectedRegion = region;
+    this.counriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
     });
   }
