9e7268c3c682477ce6ba95f813f8ef6aa0a559cf
varscript <fvargas.edu@gmail.com>
2024-05-08 15:06:58 -0500

    subscription cleaning

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,13 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject, Subscribable, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``,
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -18,15 +26,17 @@ export class SearchBoxComponent implements OnInit {
   @Output()
   public onDebounce = new EventEmitter<string>();
 
-
   ngOnInit(): void {
-    this.debouncer
-    .pipe(
-      debounceTime(300)
-    )
-    .subscribe(value => {
-      this.onDebounce.emit(value)
-    });
+    this.debouncerSubscription = this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+
   }
 
   emitValue(value: string): void {
